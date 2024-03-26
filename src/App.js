ca210c1e07956b6c757859e9bb818d60a00e085e
sanyam <sanyamnarang40@gmail.com>
2024-03-26 10:01:11 +0530

    frontend updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
-import { Routes, Route} from 'react-router-dom';
-import HomePage from './screens/HomePage';
+import React from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
   return (
-    <>
-    <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        {/* <Route path='/SignUp' element={<LoginPage/>}/> */}
-    </Routes>
-    </>
+    <div className="container">
+      <h1>Ultimate DevOps CI/CD Pipeline Project</h1>
+      <div className="details">
+        <h2>Sanyam</h2>
+        <p>Email: <a href="mailto:sanyamnarang40@gmail.com">sanyamnarang40@gmail.com</a> | Phone: +91 9017794903</p>
+        <p>GitHub: <a href="https://github.com/sanyam40">github.com/sanyam40</a> | LinkedIn: <a href="https://linkedin.com/in/sanyam40/">linkedin.com/in/sanyam40/</a> | Leetcode: <a href="https://leetcode.com/sanyam40">leetcode.com/sanyam40</a> | Portfolio: <a href="https://sanyam.online">sanyam.online</a></p>
+      </div>
+      <h2>Summary</h2>
+      <p>This CI/CD Pipeline Includes the concept of Jenkins, Git, Docker, kubernetes etc.</p>
+      
+      <h2>Skills</h2>
+      <ul>
+        <li>Programming Languages: Java, C++</li>
+        <li>Frameworks: MERN Stack (Express.js, React.js, Node.js), Java Development (Spring Boot, Spring MVC)</li>
+        <li>AppDev: React Native (Basic)</li>
+        <li>DevOps: Git, Docker, Ansible, Terraform, Kubernetes, CI/CD (Jenkins, Github Actions)</li>
+        <li>Databases: MySQL, MongoDB</li>
+      </ul>
+    </div>
   );
 }
 
